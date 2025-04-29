752cf83cc59d509c6c09a61d468a361a85155c85
solomonadzape <adzapesolomon@gmail.com>
2025-04-29 08:10:26 +0100

    feat: integrate Home component and clean up App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { sdk } from "@farcaster/frame-sdk";
 import { useEffect } from "react";
-import { useAccount, useConnect, useSignMessage } from "wagmi";
+// import { useAccount, useConnect, useSignMessage } from "wagmi";
+import Home from "./pages";
 
 function App() {
   useEffect(() => {
@@ -9,55 +10,58 @@ function App() {
 
   return (
     <>
-      <div>Mini App + Vite + TS + React + Wagmi</div>
-      <ConnectMenu />
+      <Home />
     </>
   );
 }
 
-function ConnectMenu() {
-  const { isConnected, address } = useAccount();
-  const { connect, connectors } = useConnect();
+// function ConnectMenu() {
+//   const { isConnected, address } = useAccount();
+//   const { connect, connectors } = useConnect();
 
-  if (isConnected) {
-    return (
-      <>
-        <div>Connected account:</div>
-        <div>{address}</div>
-        <SignButton />
-      </>
-    );
-  }
+//   if (isConnected) {
+//     return (
+//       <>
+//         <div>Connected account:</div>
+//         <div>{address}</div>
+//         <SignButton />
+//       </>
+//     );
+//   }
 
-  return (
-    <button type="button" onClick={() => connect({ connector: connectors[0] })}>
-      Connect
-    </button>
-  );
-}
+//   return (
+//     <button type="button" onClick={() => connect({ connector: connectors[0] })}>
+//       Connect
+//     </button>
+//   );
+// }
 
-function SignButton() {
-  const { signMessage, isPending, data, error } = useSignMessage();
+// function SignButton() {
+//   const { signMessage, isPending, data, error } = useSignMessage();
 
-  return (
-    <>
-      <button type="button" onClick={() => signMessage({ message: "hello world" })} disabled={isPending}>
-        {isPending ? "Signing..." : "Sign message"}
-      </button>
-      {data && (
-        <>
-          <div>Signature</div>
-          <div>{data}</div>
-        </>
-      )}
-      {error && (
-        <>
-          <div>Error</div>
-          <div>{error.message}</div>
-        </>
-      )}
-    </>
-  );
-}
+//   return (
+//     <>
+//       <button
+//         type="button"
+//         onClick={() => signMessage({ message: "hello world" })}
+//         disabled={isPending}
+//       >
+//         {isPending ? "Signing..." : "Sign message"}
+//       </button>
+//       {data && (
+//         <>
+//           <div>Signature</div>
+//           <div>{data}</div>
+//         </>
+//       )}
+//       {error && (
+//         <>
+//           <div>Error</div>
+//           <div>{error.message}</div>
+//         </>
+//       )}
+//     </>
+//   );
+// }
 
 export default App;
